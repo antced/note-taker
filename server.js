6aba08561c77cb35f106a7cea53d14c737f5c30b
Anthony Cedrone <antcedrone95@gmail.com>
2022-12-12 16:06:04 -0500

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const fs = require("fs");
 
 app = express();
